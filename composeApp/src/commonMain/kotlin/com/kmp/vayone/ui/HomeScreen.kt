1613f22e8148fa10aa69927aa53c924fcb7c184e
”wangbo“ <wangxiaobo@1035414320154056.onaliyun.com>
2025-12-04 17:47:14 +0800

    修复 iosModule 缺失

--- composeApp/src/commonMain/kotlin/com/kmp/vayone/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/vayone/ui/HomeScreen.kt
@@ -1,46 +1,69 @@
 package com.kmp.vayone.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kmp.vayone.data.Strings
 import com.kmp.vayone.navigation.Screen
-import com.kmp.vayone.data.CacheManager
 import com.kmp.vayone.ui.tabs.HomePage
 import com.kmp.vayone.ui.tabs.MinePage
 import com.kmp.vayone.ui.tabs.OrderPage
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+import theme.C_2B2621
+import theme.C_B4B0AD
+import vayone.composeapp.generated.resources.Res
+import vayone.composeapp.generated.resources.home_normal
+import vayone.composeapp.generated.resources.home_select
+import vayone.composeapp.generated.resources.mine_normal
+import vayone.composeapp.generated.resources.mine_select
+import vayone.composeapp.generated.resources.order_normal
+import vayone.composeapp.generated.resources.order_select
 
 @Composable
 fun HomeScreen(navigate: (Screen) -> Unit) {
-    var selectedTab by remember { mutableStateOf(0) }
+    var selectedIndex by remember { mutableStateOf(0) }
 
     Scaffold(
+        modifier = Modifier.fillMaxSize().navigationBarsPadding(),
         bottomBar = {
-            BottomNavigation {
-                val tabs = listOf("Home", "Order", "Mine")
-
-                tabs.forEachIndexed { index, title ->
-                    BottomNavigationItem(
-                        selected = selectedTab == index,
-                        onClick = {
-                            if (index > 0 && !CacheManager.isLoggedIn()) {
-                                navigate(Screen.Login)
-                            } else {
-                                selectedTab = index
-                            }
-                        },
-                        label = { Text(title) },
-//                        icon = { Icon(Icons.Default.Home, null) }
-                    )
-                }
-            }
+            val navItems = listOf(
+                BottomNavItem(Strings["home"], Res.drawable.home_select, Res.drawable.home_normal),
+                BottomNavItem(
+                    Strings["order"],
+                    Res.drawable.order_select,
+                    Res.drawable.order_normal
+                ),
+                BottomNavItem(Strings["mine"], Res.drawable.mine_select, Res.drawable.mine_normal)
+            )
+            BottomNavigationBar(
+                items = navItems,
+                selectedIndex = selectedIndex,
+                onItemSelected = { selectedIndex = it }
+            )
         }
     ) {
-        Box(modifier = Modifier.background(Color.Blue)){
-            when (selectedTab) {
+        Box(modifier = Modifier.background(Color.Blue)) {
+            when (selectedIndex) {
                 0 -> HomePage()
                 1 -> OrderPage()
                 2 -> MinePage()
@@ -49,16 +72,51 @@ fun HomeScreen(navigate: (Screen) -> Unit) {
     }
 }
 
+data class BottomNavItem(
+    val label: String,
+    val iconSelected: DrawableResource,
+    val iconUnselected: DrawableResource,
+)
+
 @Composable
-fun BottomNavigationItem(
-    selected: Boolean,
-    onClick: () -> Unit,
-    label: @Composable () -> Unit,
-//    icon: () -> Icon
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    modifier: Modifier = Modifier,
+    selectedIndex: Int = 0,
+    onItemSelected: (index: Int) -> Unit,
 ) {
-}
-
-@Composable
-fun BottomNavigation(content: @Composable () -> Unit) {
-
+    Row(
+        modifier = modifier.height(54.dp)
+            .fillMaxWidth()
+            .background(color = Color.White),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        items.forEachIndexed { index, item ->
+            val selected = index == selectedIndex
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable { onItemSelected(index) },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Image(
+                    painter = painterResource(
+                        if (selected) item.iconSelected else item.iconUnselected
+                    ),
+                    contentDescription = item.label,
+                    modifier = Modifier.size(24.dp),
+                    alignment = Alignment.Center
+                )
+                Text(
+                    text = item.label,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = if (selected) C_2B2621 else C_B4B0AD,
+                )
+            }
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/com/kmp/vayone/ui/tabs/MinePage.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/vayone/ui/tabs/MinePage.kt
@@ -1,10 +1,101 @@
 package com.kmp.vayone.ui.tabs
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kmp.vayone.data.Strings
+import org.jetbrains.compose.resources.imageResource
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import theme.C_FC7700
+import theme.C_FFBB48
+import theme.white
+import vayone.composeapp.generated.resources.Res
+import vayone.composeapp.generated.resources.avatar
+import vayone.composeapp.generated.resources.avatar_bg
+import vayone.composeapp.generated.resources.mine_icon
 
 
 @Composable
 fun MinePage() {
-    Text("Mine")
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(white)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .height(298.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(C_FFBB48, C_FC7700)
+                    ),
+                    shape = RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)
+                )
+        ) {
+            Image(
+                modifier = Modifier.padding(top = 42.dp).size(174.dp).align(Alignment.TopEnd),
+                contentDescription = null,
+                alignment = Alignment.TopEnd,
+                painter = painterResource(Res.drawable.mine_icon)
+            )
+            Box(modifier = Modifier.padding(top = 86.dp, start = 19.dp).size(66.dp)) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    contentDescription = null,
+                    painter = painterResource(Res.drawable.avatar_bg)
+                )
+                Image(
+                    modifier = Modifier.size(60.dp).align(Alignment.Center),
+                    contentDescription = null,
+                    painter = painterResource(Res.drawable.avatar)
+                )
+            }
+            Text(
+                text = Strings["welcome_str"],
+                color = white,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.wrapContentSize().align(Alignment.TopStart)
+                    .padding(start = 92.dp, top = 91.dp)
+            )
+            Text(
+                text = "",
+                color = white,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.wrapContentSize().align(Alignment.TopStart)
+                    .padding(start = 92.dp, top = 116.dp)
+            )
+            Row(modifier = Modifier.fillMaxWidth()
+                .height(125.dp)
+                .align(Alignment.BottomCenter)
+                .background(color = white.copy(0.3f), shape = RoundedCornerShape(24.dp))) {
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreMine() {
+    MinePage()
 }
